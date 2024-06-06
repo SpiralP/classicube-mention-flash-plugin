634fd687792831c2ce10d5d76b5b8543beff5190
SpiralP <spiral.palmer@gmail.com>
2024-06-06 15:37:10 -0700

    update deps, update nix flake

--- flake.nix
+++ flake.nix
@@ -1,11 +1,45 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
   };
 
   outputs = { nixpkgs, ... }:
     let
       inherit (nixpkgs) lib;
+
+      makePackages = (pkgs:
+        let
+          rustManifest = lib.importTOML ./Cargo.toml;
+        in
+        {
+          default = pkgs.rustPlatform.buildRustPackage {
+            pname = rustManifest.package.name;
+            version = rustManifest.package.version;
+
+            src = lib.sourceByRegex ./. [
+              "^\.cargo(/.*)?$"
+              "^build\.rs$"
+              "^Cargo\.(lock|toml)$"
+              "^src(/.*)?$"
+            ];
+
+            cargoLock = {
+              lockFile = ./Cargo.lock;
+              outputHashes = {
+                "async-dispatcher-0.1.0" = "sha256-rqpQ176/PnI9vvPrwQvK3GJbryjb3hHkb+o1RyCZ3Vg=";
+                "classicube-helpers-2.0.0+classicube.1.3.6" = "sha256-V5PBZR0rj42crA1fGUjMk4rDh0ZpjjNcbMCe6bgotW8=";
+              };
+            };
+
+            buildInputs = with pkgs; [
+              xorg.libX11
+            ];
+            nativeBuildInputs = with pkgs; [
+              pkg-config
+              rustPlatform.bindgenHook
+            ];
+          };
+        });
     in
     builtins.foldl' lib.recursiveUpdate { } (builtins.map
       (system:
@@ -14,49 +48,37 @@
             inherit system;
           };
 
-          inherit (pkgs) rustPlatform;
-          package = rustPlatform.buildRustPackage {
-            name = "classicube-mention-flash-plugin";
-            src = lib.cleanSourceWith rec {
-              src = ./.;
-              filter = path: type:
-                lib.cleanSourceFilter path type
-                && (
-                  let
-                    baseName = builtins.baseNameOf (builtins.toString path);
-                    relPath = lib.removePrefix (builtins.toString ./.) (builtins.toString path);
-                  in
-                  lib.any (re: builtins.match re relPath != null) [
-                    "/Cargo.toml"
-                    "/Cargo.lock"
-                    "/src"
-                    "/src/.*"
-                  ]
-                );
-            };
-            cargoSha256 = "sha256-ArgbfdP8hP2tRFtKBsKR+kCWKMfp13PYGMj4bPQ5CsI=";
-            nativeBuildInputs = with pkgs; [
-              pkg-config
-              rustPlatform.bindgenHook
-            ];
-            buildInputs = with pkgs; [
-              xorg.libX11
-            ];
-
-            doCheck = false;
-          };
+          packages = makePackages pkgs;
         in
-        rec {
-          devShells.${system}.default = package.overrideAttrs (old: {
-            nativeBuildInputs = with pkgs; old.nativeBuildInputs ++ [
-              clippy
-              rustfmt
-              rust-analyzer
-            ];
-          });
-          packages.${system}.default = package;
-        }
-      )
+        {
+          devShells.${system} = packages // {
+            default =
+              let
+                allDrvsIn = (name:
+                  lib.lists.flatten (
+                    builtins.map
+                      (drv: drv.${name} or [ ])
+                      (builtins.attrValues packages)
+                  ));
+              in
+              pkgs.mkShell {
+                name = "dev-shell";
+                packages = with pkgs; [
+                  clippy
+                  rustfmt
+                  rust-analyzer
+                ];
+                buildInputs = allDrvsIn "buildInputs";
+                nativeBuildInputs = allDrvsIn "nativeBuildInputs";
+                propagatedBuildInputs = allDrvsIn "propagatedBuildInputs";
+                propagatedNativeBuildInputs = allDrvsIn "propagatedNativeBuildInputs";
+              };
+          };
+          packages.${system} = packages // {
+            default = pkgs.linkFarmFromDrvs "link-farm" (builtins.attrValues packages);
+          };
+        })
       lib.systems.flakeExposed);
 }
+
 

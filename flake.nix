3cf7718070b4a746509ed04dbfd8465eaa80c0df
SpiralP <spiral.palmer@gmail.com>
2026-05-07 13:54:19 -0700

    feat!: modernize plugin and switch to semantic-release
    Refactors src/ around a component dispatcher so the plugin is
    hot-reload-safe.
    
    Bumps to Rust edition 2024, classicube-sys 5.0.0,
    classicube-helpers 4.x. flake.nix moves to
    SpiralP/nix-flake-utils + makeRustPackage and locks to
    nixos-25.11.
    
    Replaces cargo-release with semantic-release: build.yml becomes a
    workflow_call matrix feeding release.yml.
    dependabot.yml added alongside the existing renovate config.
    
    BREAKING CHANGE: i686 build matrix entries removed.

--- flake.nix
+++ flake.nix
@@ -1,38 +1,17 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    flake-utils.url = "github:SpiralP/nix-flake-utils";
   };
 
-  outputs = { self, nixpkgs }:
-    let
-      inherit (nixpkgs) lib;
-
-      rustManifest = lib.importTOML ./Cargo.toml;
-
-      revSuffix = lib.optionalString (self ? shortRev || self ? dirtyShortRev)
-        "-${self.shortRev or self.dirtyShortRev}";
-
-      makePackages = (system: dev:
+  outputs = inputs@{ flake-utils, ... }:
+    flake-utils.lib.makeOutputs inputs
+      ({ lib, pkgs, makeRustPackage, dev, ... }:
         let
-          pkgs = import nixpkgs {
-            inherit system;
-          };
+          src = ./.;
 
-          defaultAttrs = {
-            pname = rustManifest.package.name;
-            version = rustManifest.package.version + revSuffix;
-
-            src = lib.sourceByRegex ./. [
-              "^\.cargo(/.*)?$"
-              "^build\.rs$"
-              "^Cargo\.(lock|toml)$"
-              "^src(/.*)?$"
-            ];
-
-            cargoLock = {
-              lockFile = ./Cargo.lock;
-              allowBuiltinFetchGit = true;
-            };
+          args = {
+            inherit src;
 
             buildInputs = with pkgs; with xorg; [
               libX11
@@ -41,39 +20,18 @@
             nativeBuildInputs = with pkgs; [
               pkg-config
               rustPlatform.bindgenHook
-            ] ++ (if dev then
-              with pkgs; ([
-                cargo-release
-                clippy
-                rust-analyzer
-                (rustfmt.override { asNightly = true; })
-              ]) else [ ]);
+            ];
+
+            useNextest = true;
           };
         in
         {
-          default = pkgs.rustPlatform.buildRustPackage defaultAttrs;
+          inherit src;
 
-          debug = (pkgs.enableDebugging {
-            inherit (pkgs) stdenv;
-            override = (attrs: pkgs.makeRustPlatform ({
-              inherit (pkgs) rustc cargo;
-            } // attrs));
-          }).buildRustPackage (
-            (defaultAttrs // {
-              pname = "${defaultAttrs.pname}-debug";
-
-              buildType = "debug";
-
-              hardeningDisable = [ "all" ];
-            })
-          );
-        }
-      );
-    in
-    builtins.foldl' lib.recursiveUpdate { } (builtins.map
-      (system: {
-        devShells.${system} = makePackages system true;
-        packages.${system} = makePackages system false;
-      })
-      lib.systems.flakeExposed);
+          default = makeRustPackage pkgs (self: args);
+          debug = makeRustPackage pkgs (self: args // {
+            buildType = "debug";
+            hardeningDisable = [ "all" ];
+          });
+        });
 }
